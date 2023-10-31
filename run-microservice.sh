25a9967889e86cf9427e07fc1056831199b90670
Giorgi Dzidziguri <giorgidzidziguri27@gmail.com>
2023-10-31 18:37:13 +0400

    add opentelemetry agent and logging as output for traces and metrics

--- run-microservice.sh
+++ run-microservice.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 mvn clean package -Dmaven.test.skip=true
+AGENT_FILE=opentelemetry-javaagent-all.jar
+if [ ! -f "${AGENT_FILE}" ]; then
+  curl -L https://github.com/aws-observability/aws-otel-java-instrumentation/releases/download/v1.28.1/aws-opentelemetry-agent.jar --output ${AGENT_FILE}
+fi
 
-java -jar target/hello-app-1.0.jar
+export OTEL_TRACES_EXPORTER=logging
+export OTEL_METRICS_EXPORTER=logging
+java -javaagent:./${AGENT_FILE} -jar target/hello-app-1.0.jar
