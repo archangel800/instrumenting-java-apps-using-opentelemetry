2682d2cd2457c8f7f07bf90393b1ce60c52506b3
Giorgi Dzidziguri <giorgidzidziguri27@gmail.com>
2023-11-02 11:39:28 +0400

    manually add tracer and span

--- src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
+++ src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
@@ -2,8 +2,13 @@ package tutorial.buildon.aws.o11y;
 
 import java.util.Objects;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +17,22 @@ public class HelloAppController {
     private static final Logger log =
         LoggerFactory.getLogger(HelloAppController.class);
 
+    @Value("otel.traces.api.version")
+    private String tracesApiVersion;
+    private final Tracer tracer =
+            GlobalOpenTelemetry.getTracer("io.opentelemetry.traces.hello", tracesApiVersion);
+
     @RequestMapping(method= RequestMethod.GET, value="/hello")
     public Response hello() {
         Response response = buildResponse();
+        Span span = tracer.spanBuilder("mySpan").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            if(response.isValid())
+            log.info("The response is valid.");
+        }
+        finally {
+            span.end();
+        }
         if (response.isValid()) {
             log.info("The response is valid.");
         }
