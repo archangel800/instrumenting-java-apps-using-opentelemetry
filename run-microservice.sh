cf7cee9605c2644b3179944fdd480f1b57aa636c
Giorgi Dzidziguri <giorgidzidziguri27@gmail.com>
2023-11-02 11:19:31 +0400

    add otel collector and configure sh

--- run-microservice.sh
+++ run-microservice.sh
@@ -6,6 +6,9 @@ if [ ! -f "${AGENT_FILE}" ]; then
   curl -L https://github.com/aws-observability/aws-otel-java-instrumentation/releases/download/v1.28.1/aws-opentelemetry-agent.jar --output ${AGENT_FILE}
 fi
 
-export OTEL_TRACES_EXPORTER=logging
-export OTEL_METRICS_EXPORTER=logging
+export OTEL_TRACES_EXPORTER=otlp
+export OTEL_METRICS_EXPORTER=otlp
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:5555
+export OTEL_RESOURCE_ATTRIBUTES=service.name=hello-app,service.version=1.0
+
 java -javaagent:./${AGENT_FILE} -jar target/hello-app-1.0.jar
